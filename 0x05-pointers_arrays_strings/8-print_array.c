de672a86a60b218d68fa5cd6f4c589324cb88fa6
Osho Emmanuel <oshoemmanuel3@gmail.com>
2022-06-13 21:18:58 -0700

    task8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,12 +10,18 @@
 void print_array(int *a, int n)
 {
 	int index;
+
 	for (index = 0; index < n; index++)
 	{
-		printf("%d", a[index]);
+		printf("%d", a[index];
+		
 		if (index == n - 1)
+		{
 			continue;
+		}
+
 		printf(",");
 	}
+
 	printf("\n");
 }
