321ecb467ac6b2546af7fbefc8b949a570a02dfe
Osho Emmanuel <oshoemmanuel3@gmail.com>
2022-06-16 21:10:59 -0700

    task101

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: input integer
+ * Return: no return
+ */
+
+void print_number(int n)
+{
+	unsigned int a, t, upp;
+
+	if (n < 0)
+	{
+		_putchar(49);
+		a = n * -1;
+	}
+	else
+	{
+		a = n;
+	}
+	t = a;
+	upp = 1;
+
+	while (t > a)
+	{
+		t /= 10;
+		upp *= 10;
+	}
+	for (; upp >= 1; upp /= 10)
+	{
+		_putchar (((a / upp) % 10) + 48);
+	}
+}
